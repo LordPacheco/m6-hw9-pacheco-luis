5e287fca848bb20dd947b0f6030a6fa2fbfd068c
LordPacheco <70926063+LordPacheco@users.noreply.github.com>
2021-10-31 22:54:34 -0400

    index js

---
+++ index.js
@@ -0,0 +1,43 @@
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults (query) {
+  var cityName = document.getElementsByClassName("search-box")[0].value;
+  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=f87c1774c20761d49dcf8dd03305bf78'
+  
+  fetch(weatherUrl)
+  .then(weather => {
+    return weather.json();
+  }).then(displayResults);
+}
+
+function displayResults (weather) {
+  console.log(weather);
+  let city = document.querySelector('.location .city');
+  city.innerText = weather.name + " , " + weather.sys.country;
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerText = Math.round(weather.main.temp);
+
+  let feelsLike = document.querySelector('.current .feelsLike');
+  feelsLike.innerText = Math.round(weather.main.feels_like);
+
+  let weather_el = document.querySelector('.current .description');
+  weather_el.innerText = weather.weather[0].main;
+  
+
+  var pic = document.createElement('img')
+  pic.src = 'https://openweathermap.org/img/w/"+weather[0].icon+".png';
+  document.getElementsByClassName('current').appendChild(pic)
+
+  //var icon = 'https://openweathermap.org/img/w/'+weather[0].icon+'.png';
+  //let wIcon = document.querySelector('.current .weatherIcon');
+  //wIcon.innerText = 'https://openweathermap.org/img/w/"+weather[0].icon+".png';
+ 
+}
